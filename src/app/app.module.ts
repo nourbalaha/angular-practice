ee9fa600014042313caf627331f4db8168fde150
Nour Balaha <nourbalaha0@gmail.com>
2020-08-29 02:34:20 +0900

    removed unnecessary code from app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Server } from './Server/Server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Server,
+    ServersComponent
   ],
   imports: [
     BrowserModule
