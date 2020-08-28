5f15e43f6122de8d796db361658ffd6e2b8d5d1c
Nour Balaha <nourbalaha0@gmail.com>
2020-08-29 02:32:51 +0900

    created servers component

---
+++ src/app/Servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = true;
+  constructor() { 
+    setTimeout(()=>{
+      this.allowNewServer = false;
+    },2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
