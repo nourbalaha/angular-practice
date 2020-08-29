bc22d111bc0582818b46d541122f8d307d7195a2
Nour Balaha <nourbalaha0@gmail.com>
2020-08-30 01:10:25 +0900

    finished ngStyle

--- src/app/Server/Server.component.ts
+++ src/app/Server/Server.component.ts
@@ -9,7 +9,15 @@ export class Server {
     serverId: number = 10;
     serverStatus: string = "offline";
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+    }
+
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === "online" ? "green" : "red";
     }
 }
